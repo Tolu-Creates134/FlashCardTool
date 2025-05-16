473d138c38b98287fd8cbbf31ad85e7118a0e7bd
Tolu Abbass <abbasstolulope@gmail.com>
2025-05-16 09:28:34 +0100

    Fixed directory layout

--- backend/FlashCardTool/FlashCardTool.Infrastructure/obj/Debug/net9.0/FlashCardTool.Infrastructure.AssemblyInfo.cs
+++ backend/FlashCardTool/FlashCardTool.Infrastructure/obj/Debug/net9.0/FlashCardTool.Infrastructure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FlashCardTool.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+abc2e83434c3bb285f362b86fbeba8c097c482cd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1b8abe4835047a317cb89836ab35ffe1d5e4c966")]
 [assembly: System.Reflection.AssemblyProductAttribute("FlashCardTool.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FlashCardTool.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
