50ed65345128034c81c1b9c6ffc72968cd32ab93
Tolu Abbass <abbasstolulope@gmail.com>
2025-05-18 13:46:40 +0100

    added gitignore

--- backend/FlashCardTool/FlashCardTool.Domian/Entities/FlashCard.cs
+++ backend/FlashCardTool/FlashCardTool.Domian/Entities/FlashCard.cs
@@ -5,7 +5,6 @@ namespace FlashCardTool.Domain.Entities;
 
 public class FlashCard : BaseEntity
 {
-    public Guid Id { get; set; } // Primary Key
     public string Question { get; set; } = string.Empty;
     public string Answer { get; set; } = string.Empty;
     public int DeckId { get; set; } // Foreign key to Deck
