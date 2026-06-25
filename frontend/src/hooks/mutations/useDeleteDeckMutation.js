60f0f5d0bdab5965106c62e512eccf5cc6210945
Tolu Abbass <abbasstolulope@gmail.com>
2026-06-25 13:51:52 +0100

    Delete behaviour

--- frontend/src/hooks/mutations/useDeleteDeckMutation.js
+++ frontend/src/hooks/mutations/useDeleteDeckMutation.js
@@ -1,5 +1,6 @@
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { deleteDeck } from "../../services/api";
+import { useNavigate } from "react-router-dom";
 
 /**
  * Deletes a deck using deckId
@@ -9,11 +10,14 @@ import { deleteDeck } from "../../services/api";
 export const useDeleteDeckMutation = (deckId) => {
     const queryClient = useQueryClient();
 
+    const navigate = useNavigate();
+
     return useMutation({
         mutationFn: async () => {
             await deleteDeck(deckId)
         },
         onSuccess: () => {
+            navigate('/home'); // navigate first
             queryClient.removeQueries({ queryKey: ['deck', deckId] });
             queryClient.removeQueries({ queryKey: ['flashcards', deckId]});
             queryClient.invalidateQueries({ queryKey: ['decks'] });
